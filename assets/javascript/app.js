fbf4fb33bb188a3a831b0118ad49aa99275cb30c
Farjana Huq <farjanahuq@dhcp-v119-102.mobile.uci.edu>
2018-11-28 21:17:01 -0800

    updated Trivia Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//create objects for each question
+//onclick function to the start button
+// Inside start button write the timer function 
+//display time left
+//display question 
+//display the possible answers
+//onclick function for the correct answer
+//if all the questions answered in 30 secs, alert win
+// if not then alert loose
+
+$(document).ready(function(){
+    console.log("hello")
+    
+
+    $("#start-button").on("click", function(){
+           $("#start-button").hide();
+           
+           $("#questionare-panel").append();
+    })
+});
