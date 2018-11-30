dc296ea51ee0a01ff055d0bcd5676c18876d6403
Farjana Huq <farjanahuq@Farjanas-MacBook-Pro.local>
2018-11-30 00:01:51 -0800

    stopwatch working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,87 +8,53 @@
 //if all the questions answered in 30 secs, alert win
 // if not then alert loose
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("hello");
 
-    //==============================================================================================
     // Global Variables
     //==============================================================================================
+    var intervalId;
+    var clockRunning = false;          // prevents the clock from being sped up unnecessarily
 
-    var time = 30;
-
-    // var countDown = function(countTime, elem){
-    //     var element = document.getElementById(elem);
-    //     element.innerHTML = countTime + " secs remaining...";
-  
-    //     if(countTime < 1){
-    //         clearTimeout(timer);
-    //         element.innerHTML = "Countdown Complete";
-    //     }  
-    //     countTime--;
-    //     var timer = setTimeout('countDown('+ countTime + ', "' + elem + '")', 1000);
-
-    // }
-    // countDown(time, "display");
-    // var countTime = 10;
     
-    // var countDown = function( ){
-    //    countTime--;
-    //    $("#display").append(countTime + " secs remaining..");
-    //     if(countTime === 0){
-            
-    //         $("#display").append("Countdown Complete!");
-    //         clearTimeout(timer);
-    //     }
-    //     var timer = setTimeout(countDown, 1000);
-       
-     
-    //     console.log(countTime);
-    // }
+    // Object
+    //==============================================================================================
+   
+    // Stopwatch to count down the time
+    var stopwatch = {
+        time: 10,
+        reset: function () {
+            stopwatch.time = 0;
 
-   //countDown(5);
-    $("#start").on("click", function(){
-           $("#start").hide();
-           stopwatch.start();
-        //countDown(5);
-          
-          // $("#questionare-panel").append();
+            $("#display").text(stopwatch.time + "secs remaining..");
+        },
+        start: function () {
+            //Use setInterval to start the count here and set the clock to running.
+            if (!clockRunning) {
+                intervalId = setInterval(stopwatch.count, 1000);
+                clockRunning = true;
+            }
+        },
+        stop: function () {
+            //Use clearInterval to stop the count here and set the clock to not be running.
+            clearInterval(intervalId);
+            clockRunning = false;
+        },
+        count: function () {
+            stopwatch.time--;
+            if (stopwatch.time === 0) {
+                stopwatch.stop();
+                $("#display").text("Countdown Complete!");
+            }
+            $("#display").text(stopwatch.time + "secs remaining..");
+        },
+    };
+    // Functions
+    //==============================================================================================
+
+    $("#start").on("click", function () {
+        $("#start").hide();
+        stopwatch.start();
     })
 
-var intervalId;
-
-// prevents the clock from being sped up unnecessarily
-var clockRunning = false;
-var stopwatch = {
-  time:10,
- 
-
-  reset: function() {
-    stopwatch.time = 0;
-   
-    $("#display").text(stopwatch.time + "secs remaining..");
-  },
-  start: function() {
-    //Use setInterval to start the count here and set the clock to running.
-    if (!clockRunning) {
-      intervalId = setInterval(stopwatch.count, 1000);
-      clockRunning = true;
-     
-    }
-  },
-  stop: function() {
-    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-    clearInterval(intervalId);
-    clockRunning = false;
-  }, 
-  count: function() {
-    stopwatch.time--;
-    if(stopwatch.time === 0){
-          stopwatch.stop;  
-        $("#display").text("Countdown Complete!"); 
-    }
-    $("#display").text(stopwatch.time + "secs remaining..");
-  },
-  
-};
 });
